077be5fb0d585a8f29cabd2314d72a17f50b607b
Harder <hardec@win.rpi.edu>
2016-11-09 19:34:33 -0500

    Sound works... BUT NOT ON WINDOWS 7git status! Should be just about done though.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+	bool gameOver;
 	public Vector3[] levelStarts;
+	public string[] levelText;
 	public int level;
 	public int currentPitch; //pitch of sound currently playing
 	public int newPitch; //pitch of sounds sent to object
+	public Hv_LimitedVisualPatch_LibWrapper wrapper;
+	public Text levelDetails;
 	// Use this for initialization
 	void Start () {
+		gameOver = false;
 		this.transform.position = levelStarts [level];
+		levelDetails.text = levelText [level];
+		wrapper.freq = 0.2f;
 	}
 	
 	// Update is called once per frame
@@ -18,24 +26,36 @@ public class GameManager : MonoBehaviour {
 		} else if (currentPitch > newPitch) {
 			currentPitch--;
 		}
+		wrapper.pitch = currentPitch;
+		if (wrapper.freq > 0) {
+			wrapper.freq -= .02f;
+		}
+		if (Input.GetKeyDown (KeyCode.R) && gameOver) { //restart game
+			gameOver = false;
+			transform.position = levelStarts [level];
+			levelDetails.text = levelText [level];
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
 		print(col.gameObject.tag);
 		if (col.gameObject.tag == "Exit") {
-			print ("LEAVE THIS PLACE");
 			progressLevel ();
+			currentPitch = 0;
+			wrapper.freq = 0.5f;
 		}
-	}
-
-	void OnTriggerStay(Collider col) {
-		print (col.gameObject.name);
 	}
 
 	void progressLevel() {
 		if (level != levelStarts.Length - 1) { //if there is a next level
 			level++;
 			transform.position = levelStarts [level];
+			levelDetails.text = levelText [level];
+		} else {
+			gameOver = true;
+			level = 0;
+			transform.position = new Vector3 (5, 15, -1);
+			levelDetails.text = "You Win! Press R to Restart.";
 		}
 	}
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,13 +15,12 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		//print (mousePos.x + ", " + mousePos.y);
 		if (mousePos.x < 2.0f && mousePos.x > -2.0f && mousePos.y < 2.0f && mousePos.y > -2.0f) {
 			if (Input.GetMouseButton(0)) {
 				indicator.transform.position = new Vector3(mousePos.x,mousePos.y,-1); //move indicator to mouse location
 				// A portion of the controller's input translated to movement
-				xPos = indicator.transform.position.x / 50; 
-				yPos = indicator.transform.position.y / 50;
+				xPos = indicator.transform.position.x / 100; 
+				yPos = indicator.transform.position.y / 100;
 			} else if (Input.GetMouseButtonUp(0)) {
 				if (Mathf.Abs (indicator.transform.position.x) < 0.1) {
 					indicator.transform.position = new Vector3 (0, indicator.transform.position.y, -1);
