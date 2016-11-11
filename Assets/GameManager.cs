5699e97cfae8a51b8827c2637bcf78b0237cf836
charder <chardergaming@gmail.com>
2016-11-11 10:03:33 -0500

    Minor Text Fixes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -38,7 +38,6 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
-		print(col.gameObject.tag);
 		if (col.gameObject.tag == "Exit") {
 			progressLevel ();
 			currentPitch = 0;
